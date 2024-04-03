97c5d314fe0dfad02940a7d41dd39b4b1e37a71f
leandrolol <leandrofaustiinoo@hotmail.com>
2024-04-03 16:45:59 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const form = document.getElementById('form-agenda');
+const nome = [];
+let linhas = '';
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    adicionaLinha();
+    atualizaTabela();
+});
+
+function adicionaLinha(){
+    const inputNomeContato = document.getElementById('nome-contato');
+    const inputTelefoneContato = document.getElementById('telefone-contato');
+
+    nome.push(inputNomeContato.value);
+    nome.push(inputTelefoneContato.value);
+
+    let linha = '<tr>';
+    linha += `<td>${inputNomeContato.value}</td>`;
+    linha += `<td>${inputTelefoneContato.value}</td>`;
+    linha += '</tr>';
+
+    linhas += linha;
+
+    inputNomeContato.value = '';
+    inputTelefoneContato.value = '';
+}
+
+function atualizaTabela(){
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
